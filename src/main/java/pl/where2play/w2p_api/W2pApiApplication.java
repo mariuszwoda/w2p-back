b3db9c28bf38202fb8d99f70b5affbb152047592
mariusz woda <mariusz.woda@gmail.com>
2025-04-18 23:42:00 +0200

    empty spring app

---
+++ src/main/java/pl/where2play/w2p_api/W2pApiApplication.java
@@ -0,0 +1,13 @@
+package pl.where2play.w2p_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class W2pApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(W2pApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/where2play/w2p_api/W2pApiApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.where2play.w2p_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class W2pApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
